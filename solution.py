0201ee1336b2598a3ad19fd2d1a53addcd3485e1
SereneYe <sereneye130@gmail.com>
2024-08-13 12:28:22 +1000

    have trouble on case 5

--- solution.py
+++ solution.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import sys
 from constants import *
 from environment import *
 from state import State
 import heapq
+from typing import List, Tuple, Optional, Any
 
 """
 solution.py
@@ -15,6 +18,19 @@ COMP3702 2024 Assignment 1 Support Code
 """
 
 
+def filter_widget_dict(widget_dict, center_dict):
+    center_types = set(center_dict.values())
+
+    # Create a copy of widget_dict for iteration, to avoid modifying a dictionary while iterating it.
+    widget_dict_copy = widget_dict.copy()
+
+    # Iterate through widget_dict_copy
+    for widget_location, widget_type in widget_dict_copy.items():
+        # If the type of widget is not in center_dict, remove it from widget_dict
+        if widget_type not in center_types:
+            del widget_dict[widget_location]
+
+
 class Solver:
 
     def __init__(self, environment, loop_counter):
@@ -25,65 +41,32 @@ class Solver:
         # NOTE: avoid performing any computationally expensive heuristic preprocessing operations here - use the preprocess_heuristic method below for this purpose
         #
 
-    # === Uniform Cost Search ==========================================================================================
     def solve_ucs(self):
         """
         Find a path which solves the environment using Uniform Cost Search (UCS).
         :return: path (list of actions, where each action is an element of BEE_ACTIONS)
         """
-
-        #
-        #
-        # TODO: Implement your UCS code here
-        #
-        # === Important ================================================================================================
-        # To ensure your code works correctly with tester, you should include the following line of code in your main
-        # search loop:
-        #
-        # self.loop_counter.inc()
-        #
-        # e.g.
-        # while loop_condition(): // (While exploring frontier)
-        #   self.loop_counter.inc()
-        #   ...
-        #
-        # ==============================================================================================================
-        #
-        #
-        visited = set()
-        parents = {}
-        frontier = []
         initial_state = self.environment.get_init_state()
-        heapq.heappush(frontier, (0, initial_state))
+        visited = {initial_state: 0}
+        frontier = [StateNode(0, initial_state, None, None)]
+        heapq.heapify(frontier)
 
         while frontier:
             self.loop_counter.inc()
-            cost, state = heapq.heappop(frontier)
-
+            node = heapq.heappop(frontier)
+            state = node.state
             if self.environment.is_solved(state):
-                path = []
-                while state != initial_state:
-                    state, action = parents[state]
-                    path.append(action)
-                path.reverse()
-                return path
+                return node.get_path()
 
-            if state in visited:
-                continue
-
-            visited.add(state)
-
-            for action in BEE_ACTIONS:
-                success, cur_cost, new_state = self.environment.perform_action(state, action)
-                if success and new_state not in visited:
-                    parents[new_state] = (state, action)
-                    heapq.heappush(frontier, (cost + cur_cost, new_state))
+            successors = node.get_successors()
+            for s in successors:
+                if s.state not in visited.keys() or s.path_cost < visited[s.state]:
+                    visited[s.state] = s.path_cost
+                    heapq.heappush(frontier, s)
 
         return []
 
-
-    # === A* Search ====================================================================================================
-
+    # === A* Search =========================================================================
     def preprocess_heuristic(self):
         """
         Perform pre-processing (e.g. pre-computing repeatedly used values) necessary for your heuristic,
@@ -102,51 +85,92 @@ class Solver:
         pass
 
     def compute_heuristic(self, state):
-        """
-        Compute a heuristic value h(n) for the given state.
-        :param state: given state (GameState object)
-        :return a real number h(n)
-        """
+        total_distance = 0
 
-        #
-        #
-        # TODO: Implement your heuristic function for A* search here.
-        #
-        # You should call this method from your solve_a_star method (e.g. every time you need to compute a heuristic
-        # value for a state).
-        #
+        if state.is_on_edge():
+            total_distance += 10
 
-        pass
+        if state.is_next_to_obstacle():
+            total_distance += 10
+
+        center_dict = state.get_target_centers()
+        widget_dict = {}
+
+        for i in range(len(state.widget_centres)):
+            widget_dict[state.widget_centres[i]] = state.environment.widget_types[i]
+
+        for widget_location, widget_type in widget_dict.items():
+            min_distance = float('inf')
+            optimal_center = None
+
+            for center, center_type in center_dict.items():
+                if center_type == widget_type:
+                    distance = abs(center[0] - widget_location[0]) + abs(center[1] - widget_location[1])
+                    if distance < min_distance:
+                        min_distance = distance
+                        optimal_center = center
+
+            if optimal_center and min_distance == 0:
+                del center_dict[optimal_center]
+
+            total_distance += min_distance
+
+
+        return total_distance/5
+
+
 
     def solve_a_star(self):
-        """
-        Find a path which solves the environment using A* search.
-        :return: path (list of actions, where each action is an element of BEE_ACTIONS)
-        """
+        initial_state = self.environment.get_init_state()
+        visited = {initial_state: 0}
+        frontier = [StateNode(0, initial_state, None, None)]
 
-        #
-        #
-        # TODO: Implement your A* search code here
-        #
-        # === Important ================================================================================================
-        # To ensure your code works correctly with tester, you should include the following line of code in your main
-        # search loop:
-        #
-        # self.loop_counter.inc()
-        #
-        # e.g.
-        # while loop_condition(): //
-        #   self.loop_counter.inc()
-        #   ...
-        #
-        # ==============================================================================================================
-        #
-        #
+        while frontier:
+            self.loop_counter.inc()
+            node = heapq.heappop(frontier)
+            state = node.state
+            if self.environment.is_solved(state):
+                return node.get_path()
 
-        pass
+            successors = node.get_successors()
+            for s in successors:
+                heuristic_cost = self.compute_heuristic(s.state)
+                full_cost = s.path_cost + heuristic_cost
+                if s.state not in visited.keys() or full_cost < visited[s.state]:
+                    visited[s.state] = full_cost
+                    heapq.heappush(frontier, StateNode(full_cost, s.state, node, s.action_from_parent))
 
-    #
-    #
-    # TODO: Add any additional methods here
-    #
-    #
+        return []
+
+
+
+
+class StateNode:
+    def __init__(self, path_cost: float, state: Any, parent: Optional[StateNode],
+                 action_from_parent: Optional[str] = None):
+        self.path_cost = path_cost
+        self.state = state
+        self.parent = parent
+        self.action_from_parent = action_from_parent
+
+    def __lt__(self, other: StateNode) -> bool:
+        return self.path_cost < other.path_cost
+
+    def get_path(self):
+        path = []
+        cur = self
+        while cur.action_from_parent is not None:
+            path.append(cur.action_from_parent)
+            cur = cur.parent
+        path.reverse()
+        return path
+
+    def get_successors(self):
+        successors = []
+        for a in BEE_ACTIONS:
+            success, cost, next_state = self.state.environment.perform_action(self.state, a)
+            if success:
+                successors.append(StateNode(self.path_cost + cost, next_state, self, a))
+        return successors
+
+
--- state.py
+++ state.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+
+import random
+
 from constants import *
+from typing import List, Tuple, Optional, Any
 
 """
 state.py
@@ -7,6 +12,16 @@ This file contains a class representing a game environment state. You should mak
 
 COMP3702 2024 Assignment 1 Support Code
 """
+
+
+def get_neighbor_coords(row, col):
+    if col % 2 == 0:
+        neighbors = [(row - 1, col - 1), (row - 1, col), (row - 1, col + 1), (row, col + 1), (row + 1, col),
+                     (row, col - 1)]
+    else:
+        neighbors = [(row, col - 1), (row - 1, col), (row, col + 1), (row + 1, col - 1),
+                     (row + 1, col), (row + 1, col + 1)]
+    return neighbors
 
 
 class State:
@@ -66,22 +81,64 @@ class State:
         return hash((self.BEE_posit, self.BEE_orient, self.widget_centres, self.widget_orients))
 
     def deepcopy(self):
-        return State(self.environment, self.BEE_posit, self.BEE_orient, self.widget_centres, self.widget_orients,
-                     force_valid=self.force_valid)
+        return State(self.environment, self.BEE_posit, self.BEE_orient, self.widget_centres,
+                     self.widget_orients, force_valid=self.force_valid)
 
-    def get_successors(self, state):
-        """
-        Get the successors of the given state.
-        return: a list of (action, new_state) pairs
-        """
-        successors = []
+    def is_bee_adjacent(self):
+        for center in self.widget_centres:
+            if abs(self.BEE_posit[0] - center[0]) <= 1 and abs(self.BEE_posit[1] - center[1]) <= 1:
+                return True
+        return False
 
-        for action in BEE_ACTIONS:
-            success, new_state = self.environment.perform_action(state, action)
-            if success:
-                successors.append((action, new_state))
+    def is_on_edge(self):
+        return self.BEE_posit[0] == 0 or self.BEE_posit[0] == self.environment.n_rows - 1 or \
+               self.BEE_posit[1] == 0 or self.BEE_posit[1] == self.environment.n_cols - 1
 
-        return successors
+    def is_next_to_obstacle(self):
+        row, col = self.BEE_posit
+        neighbors = get_neighbor_coords(row, col)
+        for dr, dc in neighbors:
+            neighbour_row, neighbour_col = row + dr, col + dc
+
+            if not (0 <= neighbour_row < self.environment.n_rows and 0 <= neighbour_col < self.environment.n_cols):
+                continue
+
+            if self.environment.obstacle_map[neighbour_row][neighbour_col] == 1:
+                return True
+
+        return False
+
+    def get_target_centers(self):
+        target_list_copy = self.environment.target_list.copy()
+        center_points = {}
+        nums = sorted((int(num) for num in self.environment.widget_types), reverse=True)
+        for n in nums:
+            target_found = False  # Set a flag
+            for target in target_list_copy.copy():
+                row, col = target
+                neighbors = get_neighbor_coords(row, col)
+                neighbor_count = sum(1 for neighbor in neighbors if neighbor in target_list_copy)
+                if neighbor_count >= n - 1:
+                    center_points[target] = str(n)
+                    target_list_copy.remove(target)
+                    remove_neighbors_count = 0
+                    for neighbor in neighbors:
+                        if neighbor in target_list_copy:
+                            target_list_copy.remove(neighbor)
+                            remove_neighbors_count += 1
+                        if remove_neighbors_count >= n - 1:
+                            break
+                    target_found = True
+                    break
+            if target_found:
+                continue
+
+        while len(center_points) < self.environment.n_widgets and target_list_copy:
+            random_point = random.choice(target_list_copy)
+            center_points[random_point] = len(target_list_copy)
+            target_list_copy.remove(random_point)
+
+        return center_points
 
 
 
