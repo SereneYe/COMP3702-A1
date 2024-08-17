44b51f5130737abdf89372c31697d3aee47f54ba
SereneYe <sereneye130@gmail.com>
2024-08-17 11:20:42 +1000

    Optimized the deepcopy to improve the efficiency

--- solution.py
+++ solution.py
@@ -124,7 +124,7 @@ class Solver:
                         if distance < min_distance:
                             min_distance = distance
 
-                total_distance += min_distance * 0.45
+                total_distance += min_distance * 0.49
 
             return total_distance
 
